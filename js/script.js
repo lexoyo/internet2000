5d42beda1dc711d2bea79b113d015482cff153bd
Moly <64364914+molyrichez@users.noreply.github.com>
2020-08-12 14:50:08 +0200

    Batch update

---
+++ js/script.js
@@ -0,0 +1,4 @@
+
+
+    window.silex = window.silex || {}
+    window.silex.data = {"site":{"width":1200},"pages":[{"id":"page-home","displayName":"home","link":{"linkType":"LinkTypePage","href":"#!page-home"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"page-what","displayName":"what","link":{"linkType":"LinkTypePage","href":"#!page-what"},"canDelete":true,"canRename":true,"canMove":true,"canProperties":true,"opened":false}]}
